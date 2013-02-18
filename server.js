908e73beb9d4846ab7c168f9f952808a9b02d9d5
Lars Nyström <nystrom.lasse@gmail.com>
2013-02-18 22:22:37 +0100

    Templating with edit_area

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ var App = function(){
   self.routes['getTemplates'] = function(req, res) {
     self.db.collection('templates', function(err, collection) {
       collection.find().toArray(function(err, templates) {
+        mu.clearCache();
         var stream = mu.compileAndRender(self.mustacheTemplates + '/templates.html', { "templates": templates });
         util.pump(stream, res);
       });
@@ -107,6 +108,24 @@ var App = function(){
         res.send({ 'error': 'An error has occured'});
       };
     })
+  };
+
+  self.routes['adddTemplates'] = function(req, res) {
+    console.log('Req: ' + req);
+    console.log('Body: ' + req.body);
+
+    var template = req.body;
+    console.log('Adding template: ' + JSON.stringify(template));
+    self.db.collection('templates', function(err, collection) {
+      collection.insert(template, { safe: true }, function(err, result) {
+        if (err) {
+          res.send({ 'error': 'An error has ocurred'});
+        } else {
+          console.log('Success' + JSON.stringify(result[0]));
+          res.send(result[0]);
+        }
+      });
+    });
   };
 
   // Webapp urls
@@ -128,6 +147,7 @@ var App = function(){
   self.app.post('/names', self.routes['addNames']);
   self.app.post('/packitem', self.routes['addPackItem']);
   self.app.get('/templates', self.routes['getTemplates']);
+  self.app.post('/templates', self.routes['addTemplates']);
 
   // Logic to open a database connection. We are going to call this outside of app so it is available to all our functions inside.
 
